65e0cdc2691691b55a38dde28960d42ddac768c3
nahsooyeon <nahsooyeon@gmail.com>
2025-01-09 15:58:47 +0900

    fix: 타이머 상태 전역으로 관리

---
+++ src/app/context/Countdown/index.tsx
@@ -0,0 +1,67 @@
+// CountdownContext.tsx
+"use client";
+
+import React, { createContext, useContext, useEffect, useRef, useState } from "react";
+
+interface CountdownContextProps {
+	isCountdown: number | null;
+	count: number;
+	start: () => void;
+	stop: () => void;
+	reset: () => void;
+	remaining: number;
+	setCount: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const CountdownContext = createContext<CountdownContextProps | undefined>(undefined);
+
+export const CountdownProvider: React.FC<{ children: React.ReactNode; }> = ({ children }) => {
+	const countdown = 180000; // Default countdown value
+	const interval = 1000; // Default interval value
+
+	const intervalRef = useRef<number>(-1);
+	const [count, setCount] = useState<number>(countdown);
+	const [isCountdown, setIsCountdown] = useState<number | null>(null);
+
+	useEffect(() => {
+		if (isCountdown === null) {
+			return;
+		}
+
+		intervalRef.current = window.setInterval(() => setCount((prev) => prev - interval), interval);
+
+		return () => {
+			window.clearInterval(intervalRef.current);
+		};
+	}, [interval, isCountdown]);
+
+	useEffect(() => {
+		if (count <= 0) {
+			setIsCountdown(null);
+			setCount(0);
+		}
+	}, [count]);
+
+	const start = () => setIsCountdown(interval);
+	const stop = () => setIsCountdown(null);
+	const reset = () => {
+		setCount(countdown);
+		setIsCountdown(null);
+	};
+
+	const remaining = countdown - count;
+
+	return (
+		<CountdownContext.Provider value={{ isCountdown, count, start, stop, reset, remaining, setCount }}>
+			{children}
+		</CountdownContext.Provider>
+	);
+};
+
+export const useCountdownContext = (): CountdownContextProps => {
+	const context = useContext(CountdownContext);
+	if (!context) {
+		throw new Error("useCountdownContext must be used within a CountdownProvider");
+	}
+	return context;
+};
--- src/app/play/sections/widgets/Timer.tsx
+++ src/app/play/sections/widgets/Timer.tsx
@@ -1,14 +1,35 @@
 "use client";
 
-import { useCountdown } from "@/app/hooks/useTimer";
+import { useCountdownContext } from "@/app/context/Countdown";
+import { cn } from "@/app/utils/tailwind";
+import dayjs from "dayjs";
+import { FunctionComponent, useRef } from "react";
 
-const TimerWidget = () => {
-	const { isCountdown, count } = useCountdown(60);
+
+const TimerWidget: FunctionComponent = () => {
+	const progressBarRef = useRef<HTMLDivElement>(null);
+	const { count, isCountdown } = useCountdownContext();
+	
+
+	
 	return (
 		<div className={"bg-[#2ADCF9] w-full h-7"}>
-			{isCountdown && <div
-				style={{ width: `${(count / 60) * 100}%` }}
-				className={"bg-black z-1"} />}
+			{isCountdown &&
+				<div className={"relative"}>
+					<div
+						ref={progressBarRef}
+						style={{ width: `${((180000 - count) / 180000) * 100}%` }}
+						className={"bg-black z-1 h-7 rounded-r-lg"} />
+					<div
+						style={{
+							left: progressBarRef.current?.offsetWidth ? `${(progressBarRef.current.offsetWidth - 20) / 16}rem` : "0",
+						}}
+						className={cn("absolute z-10 bg-white lg:px-3 -bottom-5 lg:-bottom-8  shadow-bottom z-2 h-7 lg:h-10 rounded-lg flex flex-col items-center justify-center")}>
+						{/* 삼각형 화살표 추가 */}
+						<div className={"text-white absolute z-10 -top-4 left-1/3"}>▲</div>
+						<div className={"text-base lg:text-lg"}>{dayjs(count).format("mm:ss:SSS")}</div>
+					</div>
+				</div>}
 		</div>
 	);
 };
