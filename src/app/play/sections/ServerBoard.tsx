f6dac03cea0f390c4990e4d33b4102b9043e728f
nahsooyeon <nahsooyeon@gmail.com>
2025-01-10 09:50:29 +0900

    hotfix: 게임 보드 API가 캐싱되어있는 오류 수정

--- src/app/play/sections/ServerBoard.tsx
+++ src/app/play/sections/ServerBoard.tsx
@@ -1,27 +1,22 @@
 import { BoardResDto } from "@/dto/board.res.dto";
-import ClientWordBoard from "./ClientWordBoard";
+import ClientWordSearchDiv from "./ClientWordBoard";
 
 const getServerSideBoard = async () => {
-	try {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/game`, {
-			method: 'POST',
-			body: JSON.stringify({
-				seed: Math.random()
-			}),
-		});
-		const data: BoardResDto = await response.json();
-		return data;
-	} catch (error) {
-		console.error(error);
-		throw new Error('Failed to fetch board data');
-	}
-
+	const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/game`, {
+		method: 'POST',
+		body: JSON.stringify({
+			seed: Math.random()
+		}),
+		cache: 'no-cache',
+	});
+	const data: BoardResDto = await response.json();
+	return data;
 };
 
 const ServerSideBoard = async () => {
 	const board = await getServerSideBoard();
 
-	return <ClientWordBoard initialBoardData={board} />;
+	return <ClientWordSearchDiv initialBoardData={board} />;
 };
 
 export default ServerSideBoard;
