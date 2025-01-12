207687c5cf7fa048a551481d03c1aca0d5e65405
nahsooyeon <nahsooyeon@gmail.com>
2025-01-12 21:51:52 +0900

    feat: 랭킹 등록 UI 개발 및 API 연동, 점수 계산 로직 추가

--- src/app/play/sections/widgets/GameInfo.tsx
+++ src/app/play/sections/widgets/GameInfo.tsx
@@ -11,14 +11,14 @@ const GameInfoWidget: FunctionComponent = () => {
 	return <div className={"bg-primary gap-4 lg:gap-11 px-5 lg:px-10 py-5 w-full flex"}>
 		<div className={"flex flex-col gap-1 items-center justify-center"}>
 			<div
-				className={"lg:size-20 size-5 p-5 bg-white shadow-bottom rounded-2xl flex items-center justify-center"}>
+				className={"lg:size-20 h-5 p-5 bg-white shadow-bottom rounded-2xl flex items-center justify-center"}>
 				<span className={"text-black text-3xl leading-6 font-normal text-center"}>{level}</span>
 			</div>
 			<p className={"text-base lg:text-2xl"}>Level</p>
 		</div>
 		<div className={"flex w-full flex-grow  flex-col gap-1 items-center justify-center"}>
 			<div
-				className={"lg:size-20 h-5 w-full shadow-bottom flex-grow bg-white p-5 rounded-2xl flex items-center justify-end"}>
+				className={"lg:h-20 h-5 w-max shadow-bottom flex-grow bg-white p-5 rounded-2xl flex items-center justify-end"}>
 				<div className={"text-black text-4xl leading-6 font-normal text-right"}>{(points).toLocaleString()}</div>
 			</div>
 			<p className={"text-base lg:text-2xl"}>Points</p>
