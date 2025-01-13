1bf061af17e8881200dc0630e1f0f03cf958d99b
nahsooyeon <nahsooyeon@gmail.com>
2025-01-13 11:14:06 +0900

    feat: useActionState 적용

--- src/app/play/sections/widgets/RankForm.tsx
+++ src/app/play/sections/widgets/RankForm.tsx
@@ -1,10 +1,16 @@
 
+"use client";
 import { GameInfoStore } from "@/app/store/GameInfo";
 import { Input } from "@/components/Input";
 import { modalStore } from "@/context/Modal";
 import dayjs from "dayjs";
-import { FormEvent, FunctionComponent } from "react";
+import { FunctionComponent, useActionState } from "react";
 import { useStore } from "zustand";
+
+type CreateRankReqDTO = {
+	name: string;
+	score: number;
+};
 
 interface RankFormModalProps {
 	solvedCount: number;
@@ -16,31 +22,41 @@ interface RankFormModalProps {
 
 
 const RankFormModal: FunctionComponent<RankFormModalProps> = ({ solvedCount, entireCount, id, usedTime }) => {
+	const [error, submitAction, isPending] = useActionState(
+		async (_previousState: null, formData: FormData) => {
+			const error = await createRank({
+				name: formData.get('name') as string,
+				score: totalScore,
+			});
+			if (error) {
+				throw error;
+			}
+			closeModal(id);
+			reset();
+			window.location.href = "/";
+			return null;
+
+		}, null
+	);
 	const { points, reset } = useStore(GameInfoStore);
 	const { closeModal } = useStore(modalStore);
 	const totalScore = points + (usedTime / 1000) * 100;
 
-
-	const onSubmit = async (event: FormEvent) => {
-		event.preventDefault();
-		const target = event.currentTarget as HTMLFormElement;
+	const createRank = async (data: CreateRankReqDTO) => {
 		try {
 			await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/highscore`, {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify({ name: (target.elements.namedItem('name') as HTMLInputElement).value, score: totalScore }),
-			}).then(() => {
-				closeModal(id);
-				reset();
-				window.location.href = "/";
+				body: JSON.stringify(data),
 			});
 		} catch (error) {
 			console.error(error);
+			return error;
 		}
-
 	};
+
 
 	return <div className={" flex my-auto bg-white flex-col w-[60vw] rounded-xl overflow-hidden h-[50vh]"}>
 		<div className={"text-center w-full py-4 text-4xl bg-[#7BFE7F] rounded-t-md"}>
@@ -55,12 +71,13 @@ const RankFormModal: FunctionComponent<RankFormModalProps> = ({ solvedCount, ent
 					<p className={"text-2xl font-medium"}><span className="text-xl">in</span> {dayjs(usedTime).format("mm:ss:SSS")}</p>
 				</div>
 			</div>
-			<form onSubmit={onSubmit} className={"flex gap-8 bg-white flex-col"}>
+			<form action={submitAction} className={"flex gap-8 bg-white flex-col"}>
 				<label htmlFor="name" className="flex flex-col items-center">
 					<p className="text-center text-lg">name</p>
 					<Input name={"name"} type="text" maxLength={10} className="rounded-2xl w-1/2 border-none text-2xl bg-[#F5F5F5] shadow-bottom " placeholder="Enter your name" />
 				</label>
-				<button className={"bg-[#7BFE7F] py-5 text-3xl mt-auto w-full shadow-bottom"} type="submit">
+				{error && <p className="text-red-500 text-center">{error}</p>}
+				<button disabled={isPending} className={"bg-[#7BFE7F] py-5 text-3xl mt-auto w-full shadow-bottom"} type="submit">
 					CLOSE
 				</button>
 			</form>
